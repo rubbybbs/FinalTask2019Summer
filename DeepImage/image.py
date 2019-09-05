d4d5cd179b8249149d9d63b1908202f2a6de079b
rubby <rubbybbs1324@gmail.com>
2019-09-05 19:35:07 +0800

    Version1.0

--- DeepImage/image.py
+++ DeepImage/image.py
@@ -1,7 +1,7 @@
 from PIL import Image
 
+from object_detection import image_process
 
 def process(in_url, out_url1, out_url2):
-    img = Image.open(in_url)
-    img.save(out_url1)
-    img.save(out_url2)
+
+    image_process.run_output(in_url, out_url1)
--- static/js/upload.js
+++ static/js/upload.js
@@ -8,12 +8,11 @@ $(function () {
   $("#fileupload").fileupload({
     dataType: 'json',
     done: function (e, data) {
-      let stylesheet = 'max-width: 400px; max-height: 400px;'
       if (data.result.is_valid) {
         $("#gallery tbody").prepend(
-            "<tr><td><img src='" + data.result.url + "' style="+stylesheet+"></td>\
-            \<td><img src='" + data.result.output_url1 + "'style="+stylesheet+"></td>" +
-            "<td><img src='" + data.result.output_url2 + "'style="+stylesheet+"></td></tr>"
+            "<tr><td><img src='" + data.result.url + "' style='max-width: 350px; max-height: 350px;'></td>\
+            \<td><img src='" + data.result.output_url1 + "'style='max-width: 350px; max-height: 350px;'></td>" +
+            "<td><img src='" + data.result.output_url2 + "'style='max-width: 350px; max-height: 350px;'></td></tr>"
         )
       }
     }
